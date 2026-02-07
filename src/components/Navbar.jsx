c9527ea4992cf525f86adcf31650ad07491687bb
clizzix <godsilla1993@gmail.com>
2026-02-07 14:04:43 +0100

    Hide LogIn and SignUp when user is LoggedIn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router';
+import { NavLink, Link, useLocation, useNavigate } from 'react-router';
 
 const Navbar = () => {
+    useLocation();
+    const navigate = useNavigate();
+    const isLoggedIn = !!localStorage.getItem('token');
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/login');
+    };
+
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="flex-1">
@@ -22,26 +31,34 @@ const Navbar = () => {
                             Home
                         </NavLink>
                     </li>
-                    <li>
-                        <NavLink
-                            to="login"
-                            className={({ isActive }) =>
-                                isActive ? 'bg-primary' : ''
-                            }
-                        >
-                            Log In
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="signup"
-                            className={({ isActive }) =>
-                                isActive ? 'bg-primary' : ''
-                            }
-                        >
-                            Sign Up
-                        </NavLink>
-                    </li>
+                    {!isLoggedIn ? (
+                        <>
+                            <li>
+                                <NavLink
+                                    to="login"
+                                    className={({ isActive }) =>
+                                        isActive ? 'bg-primary' : ''
+                                    }
+                                >
+                                    Log In
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="signup"
+                                    className={({ isActive }) =>
+                                        isActive ? 'bg-primary' : ''
+                                    }
+                                >
+                                    Sign Up
+                                </NavLink>
+                            </li>
+                        </>
+                    ) : (
+                        <li>
+                            <button onClick={handleLogout}>Logout</button>
+                        </li>
+                    )}
                     <li>
                         <NavLink
                             to="/protected"
