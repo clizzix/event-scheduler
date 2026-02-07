a913b02508d3aede3490ad81c543c3f113aaed88
clizzix <godsilla1993@gmail.com>
2026-02-07 14:49:02 +0100

    Added EventCardList and EventCard Logic

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const EventCard = () => {
-    return <div></div>;
+const EventCard = ({ event }) => {
+    return (
+        <div className="flex flex-col gap-4 bg-base-300 p-4 m-4 rounded-md border border-black shadowm-md">
+            <div>
+                <h1 className="text-3xl text-accent font-bold">
+                    {event.title}
+                </h1>
+                <p className="italic">{event.description}</p>
+            </div>
+            <div>
+                <p className="text-secondary">
+                    {new Date(event.date).toLocaleString(undefined, {
+                        year: 'numeric',
+                        month: 'short',
+                        day: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric',
+                        timeZoneName: 'short',
+                    })}
+                </p>
+                <p>@{event.location}</p>
+            </div>
+        </div>
+    );
 };
 
 export default EventCard;
