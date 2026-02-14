19c01cef7fb779af97dd43924be84df89aeab2e3
clizzix <godsilla1993@gmail.com>
2026-02-14 08:49:54 +0100

    implemented fetching upcoming events on Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import EventCard from '../components/EventCard';
 
 const Home = () => {
-    return <div></div>;
+    const [isLoading, setIsLoading] = useState(false);
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const fetchEvents = async () => {
+            try {
+                const res = await fetch(
+                    `${import.meta.env.VITE_BACKEND_URL}/api/events/upcoming`,
+                );
+                const data = await res.json();
+                setEvents(data.results);
+                console.log(data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchEvents();
+    }, []);
+
+    return (
+        <div>
+            {events ? (
+                events.map((event) => (
+                    <EventCard key={event.id} event={event} />
+                ))
+            ) : (
+                <div> No upcoming Events....</div>
+            )}
+        </div>
+    );
 };
 
 export default Home;
